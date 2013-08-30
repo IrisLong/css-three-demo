4a4b2c4441e0aad117804143060430e3059b14f6
Jay Weeks <jay.patrick.weeks@gmail.com>
2013-08-30 11:17:19 -0700

    Add content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*global console, jQuery, THREE*/
+/*global console, jQuery, THREE, TWEEN, sh_highlightDocument*/
 (function (Three, $) {
 	"use strict";
 
@@ -28,10 +28,11 @@
 
 		this.window = $(window);
 		this.mouse = new V2();
+		this.position = new V3();
+		this.target = new V3();
 
 		this.camera = params.camera;
 		this.dims = params.dims;
-		this.target = params.target;
 		this.camScale = params.camScale;
 		this.camMin = params.camMin;
 		this.onUpdate = params.onUpdate;
@@ -45,10 +46,21 @@
 			this.mouse.set(event.clientX - dims.x / 2, event.clientY - dims.y / 2);
 		},
 
+		setTarget: function (position) {
+			this.target.copy(position);
+
+			new TWEEN.Tween(this.position)
+				.to(this.target, 500)
+				.easing(TWEEN.Easing.Cubic.Out)
+				.start();
+		},
+
 		onMouseMove: function (event) {
 			this.setMouse(event);
+		},
 
-			var target = this.target;
+		update: function () {
+			var target = this.position;
 			var camScale = this.camScale;
 			var camMin = this.camMin;
 			var mouse = this.mouse;
@@ -64,17 +76,17 @@
 			camPos.z += nz;
 
 			camera.lookAt(target);
-			camera.updateMatrix();
+			// camera.updateMatrix();
 
 			this._needsUpdate = true;
 			return false;
-		},
 
-		update: function () {
+			/*
 			if (this._needsUpdate) {
 				this.onUpdate();
 				this._needsUpdate = false;
 			}
+			*/
 		}
 	};
 
@@ -82,22 +94,25 @@
 	// -----
 
 	function DemoScene() {
-		bindMethods(this, "resize", "animate", "render");
+		bindMethods(this, "resize", "animate", "render", "hotKey");
 
 		this.el = $("#container");
+		this.document = $(document);
 		this.window = $(window);
 		this.dims = new V2();
 
+		this.slidesContent = $("#slides .slide");
+		this.slides = [];
+
 		this.camera = new Three.PerspectiveCamera(75, this.el.width() / this.el.height(), 1, 5000);
-		this.camera.position.z = 1500;
+		this.camera.position.setZ(1500);
 
 		this.scene = new Three.Scene();
 		this.controls = new CameraControls({
 			camera: this.camera,
 			dims: this.dims,
-			target: this.scene.position,
 			camScale: 0.2,
-			camMin: 200,
+			camMin: 150,
 			onUpdate: this.render
 		});
 
@@ -107,11 +122,14 @@
 
 		this.grid = new Three.Object3D();
 		this.scene.add(this.grid);
-		this.initGrid(this.grid, 4, 4, 400, 400);
+		this.initGrid(this.grid, 3, 2, 1400, 900);
 
 		this.window.on("resize", this.resize);
+		this.document.on("keydown", this.hotKey);
+
 		this.resize();
 		this.animate();
+		this.goTo(0);
 	}
 
 	DemoScene.prototype = {
@@ -119,12 +137,18 @@
 			var hWidth = (cols - 1) * width / 2;
 			var hHeight = (rows - 1) * height / 2;
 			var count = rows * cols;
-			var x, y;
+			var plane, slide, x, y;
 
 			for (var i = 0; i < count; i ++) {
 				x = i % cols * width - hWidth;
 				y = Math.floor(i / cols) * height - hHeight;
-				this.addPlane(parent, x, y);
+				plane = this.addPlane(parent, x, y);
+				slide = this.slidesContent[i];
+
+				if (slide) {
+					plane.el.html(slide);
+					this.slides.push(plane);
+				}
 			}
 		},
 
@@ -132,6 +156,7 @@
 			var el = $("<div>");
 			var object = new Three.CSS3DObject(el[0]);
 
+			object.el = el;
 			object.position.set(x, y, 0);
 			el.addClass("plane");
 			parent.add(object);
@@ -140,7 +165,9 @@
 
 		animate: function () {
 			window.requestAnimationFrame(this.animate);
+			TWEEN.update();
 			this.controls.update();
+			this.render();
 		},
 
 		render: function () {
@@ -157,9 +184,43 @@
 			this.dims.set(w, h);
 			this.renderer.setSize(w, h);
 			this.render();
+		},
+
+		hotKey: function (event) {
+			switch (event.which) {
+			case 37:
+				this.prevSlide();
+				event.preventDefault();
+				break;
+			case 39:
+				this.nextSlide();
+				event.preventDefault();
+				break;
+			}
+		},
+
+		goTo: function (index, dir) {
+			var slides = this.slides;
+			index = index + dir || 0;
+
+			if (index > slides.length - 1) { index = 0; }
+			else if (index < 0) { index = slides.length - 1; }
+
+			var slide = this.slides[index];
+			this.controls.setTarget(slide.position);
+			this.__currentSlide = index;
+		},
+
+		prevSlide: function () {
+			this.goTo(this.__currentSlide || 0, -1);
+		},
+
+		nextSlide: function () {
+			this.goTo(this.__currentSlide || 0, 1);
 		}
 	};
 
 	var demo = new DemoScene();
+	sh_highlightDocument();
 
 }(THREE, jQuery));
